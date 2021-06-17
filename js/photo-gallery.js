329962e90d07fa438af6f0eae0f9750b61af670b
Tsios Alena <alena596.tsios@yandex.ru>
2021-06-17 23:55:23 +0300

    Просмотр фотографий в полноразмерном режиме

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,10 +1,20 @@
 import {renderPictures} from './thumbnails-viewer.js';
 import {showPhoto} from './photo-viewer.js';
 
+const getShowPhotoСlosure = (photoDescription) => () => showPhoto(photoDescription);
+
+const addThumbnailClickHandler = (thumbnail, photoDescription) => {
+  thumbnail.addEventListener('click', getShowPhotoСlosure(photoDescription));
+};
+
 const createPhotoGallery = (photoDescriptionList)=> {
   if(photoDescriptionList && photoDescriptionList.length > 0){
-    showPhoto(photoDescriptionList[0]);
     renderPictures(photoDescriptionList);
+
+    const thumbnails = document.querySelectorAll('.picture__img');
+    for (let i = 0; i < thumbnails.length; i++) {
+      addThumbnailClickHandler(thumbnails[i], photoDescriptionList[i]);
+    }
   }
 };
 
