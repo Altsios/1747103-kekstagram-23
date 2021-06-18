8a63d340a6bbe7a59c9ea7292311ebfb572f5346
Tsios Alena <alena596.tsios@yandex.ru>
2021-06-18 21:17:10 +0300

    Вариант сопоставления данных и dom-элемента через поиск по url

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,19 +1,29 @@
 import {renderPictures} from './thumbnails-viewer.js';
 import {showPhoto} from './photo-viewer.js';
 
-const getShowPhotoСlosure = (photoDescription) => () => showPhoto(photoDescription);
-
-const addThumbnailClickHandler = (thumbnail, photoDescription) => {
-  thumbnail.addEventListener('click', getShowPhotoСlosure(photoDescription));
-};
+const picturesElement = document.querySelector('.pictures');
 
 const createPhotoGallery = (photoDescriptionList)=> {
+
   if(photoDescriptionList && photoDescriptionList.length > 0){
+
     renderPictures(photoDescriptionList);
 
-    const thumbnails = document.querySelectorAll('.picture__img');
-    for (let i = 0; i < thumbnails.length; i++) {
-      addThumbnailClickHandler(thumbnails[i], photoDescriptionList[i]);
+    if(picturesElement){
+      picturesElement.onclick = (evt) => {
+        const target = evt.target;
+        if(target.className === 'picture__img'){
+
+          const pictureBaseUrl = target.src;
+          const baseUrl = location.origin + location.pathname;
+          const pictureUrl = pictureBaseUrl.replace(baseUrl,'');
+
+          const photoDescription = photoDescriptionList.find((photo) => photo.url === pictureUrl);
+          if(photoDescription){
+            showPhoto(photoDescription);
+          }
+        }
+      };
     }
   }
 };
