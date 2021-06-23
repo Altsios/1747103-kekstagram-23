a165e59b8e71ef69000be545d1fcf6d5058ac778
Tsios Alena <alena596.tsios@yandex.ru>
2021-06-23 22:52:21 +0300

    Обработка Esc + очищение формы

--- js/photo-uploader-form.js
+++ js/photo-uploader-form.js
@@ -4,24 +4,37 @@ const photoUploaderForm = document.querySelector('#upload-select-image');
 const fileInputElement = photoUploaderForm.querySelector('#upload-file');
 const imgEditorElement = photoUploaderForm.querySelector('.img-upload__overlay');
 const btnUploadCancelElement = photoUploaderForm.querySelector('#upload-cancel');
+const textHashTagsElement = photoUploaderForm.querySelector('.text__hashtags');
+const textDescriptionElement = photoUploaderForm.querySelector('.text__description');
 
 let onPhotoUploaderEscKeydown = undefined;
 let onPhotoUploaderCloseClick = undefined;
 
+const onPhotoUploaderUserInputKeyDown = (evt) => {
+  evt.stopPropagation();
+};
 
-const closePhotoUploaderForm =() =>{
+const clearFormFields = () => {
+  fileInputElement.value = null;
+  textHashTagsElement.value = null;
+  textDescriptionElement.value = null;
+};
+
+const closePhotoUploaderForm = () =>{
   imgEditorElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
 
   document.removeEventListener('keydown', onPhotoUploaderEscKeydown);
   btnUploadCancelElement.removeEventListener('click', onPhotoUploaderCloseClick);
 
-  fileInputElement.value = null;
+  textHashTagsElement.removeEventListener('keydown', onPhotoUploaderUserInputKeyDown);
+  textDescriptionElement.removeEventListener('keydown', onPhotoUploaderUserInputKeyDown);
+
+  clearFormFields();
 };
 
 onPhotoUploaderEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
-    evt.preventDefault();
     closePhotoUploaderForm();
   }
 };
@@ -30,20 +43,22 @@ onPhotoUploaderCloseClick = () => {
   closePhotoUploaderForm();
 };
 
-
-const openPhotoUploaderForm =() =>{
+const openPhotoUploaderForm = () => {
   imgEditorElement.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', onPhotoUploaderEscKeydown);
   btnUploadCancelElement.addEventListener('click', onPhotoUploaderCloseClick);
+
+  textHashTagsElement.addEventListener('keydown', onPhotoUploaderUserInputKeyDown);
+  textDescriptionElement.addEventListener('keydown', onPhotoUploaderUserInputKeyDown);
 };
 
-const onInputChanged = () =>{
+const onPhotoUploaderInputChanged = () =>{
   openPhotoUploaderForm();
 };
 
-fileInputElement.addEventListener('change', onInputChanged);
+fileInputElement.addEventListener('change', onPhotoUploaderInputChanged);
 
 const setPhotoUploaderFormSubmit = () => {
 
