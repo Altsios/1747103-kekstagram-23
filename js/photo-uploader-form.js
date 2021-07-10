33c0e29e2d5767a412e804c417a95134ebffe604
Tsios Alena <alena596.tsios@yandex.ru>
2021-07-10 23:58:23 +0300

    Фото на память

--- js/photo-uploader-form.js
+++ js/photo-uploader-form.js
@@ -4,6 +4,7 @@ import {addPhotoFilterEffects, removePhotoFilterEffects} from './photo-filter.js
 
 const MAX_HASHTAG_COUNT = 5;
 const MAX_HASHTAG_LENGTH = 20;
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const HASHTAG_WRONG_SYMBOLS_PATTERN = /[^#\d\sа-яa-z]+/i;
 const HASHTAG_PATTERN = new RegExp(`^#[\\dа-яa-z]{1,${MAX_HASHTAG_LENGTH - 1}}$`, 'i');
 
@@ -13,6 +14,7 @@ const imgEditorElement = photoUploaderForm.querySelector('.img-upload__overlay')
 const btnUploadCancelElement = photoUploaderForm.querySelector('#upload-cancel');
 const textHashTagsElement = photoUploaderForm.querySelector('.text__hashtags');
 const textDescriptionElement = photoUploaderForm.querySelector('.text__description');
+const imgUploadPreviewElement = photoUploaderForm.querySelector('.img-upload__preview img');
 
 let onPhotoUploaderEscKeydown = undefined;
 let onPhotoUploaderCloseClick = undefined;
@@ -123,6 +125,21 @@ const openPhotoUploaderForm = () => {
 };
 
 const onPhotoUploaderInputChanged = () =>{
+
+  const file = fileInputElement.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      imgUploadPreviewElement.src = reader.result;
+    });
+
+    reader.readAsDataURL(file);
+  }
+
   openPhotoUploaderForm();
 };
 
